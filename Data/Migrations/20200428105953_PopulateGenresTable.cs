1e21505246d525f61ed29a83e51bcc8c1ed92fa7
Witness327 <48520309+Witness327@users.noreply.github.com>
2020-05-02 12:04:50 -0400

    Populate Genres Table

---
+++ Data/Migrations/20200428105953_PopulateGenresTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GigHub.Data.Migrations
+{
+    public partial class PopulateGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Jazz')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Blues')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Rock')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Country')");
+
+        }
+
+        //private void Sql(string v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Genres WHERE Id IN 1,2,3,4)");
+        }
+    }
+}
